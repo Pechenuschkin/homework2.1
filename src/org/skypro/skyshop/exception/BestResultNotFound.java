37498e900267c604223067e5510d40be780ae6c7
pechenuschkin <https://github.com/Pechenuschkin357@ya.ru/helloworld.git>
2025-12-18 17:13:23 +0300

    Исправленное ДЗ.

--- src/org/skypro/skyshop/exception/BestResultNotFound.java
+++ src/org/skypro/skyshop/exception/BestResultNotFound.java
@@ -3,7 +3,7 @@ package org.skypro.skyshop.exception;
 import java.io.IOException;
 
 // Создали класс исключения.
-public class BestResultNotFound extends IOException {
+public class BestResultNotFound extends Exception {
 
     public BestResultNotFound(String s) {
         super(s);
